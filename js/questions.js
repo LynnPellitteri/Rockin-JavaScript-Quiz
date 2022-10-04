60501f4663ae80d5600d74eaaeac97b9b3caa563
LynnPellitteri <lynnpellitteri@gmail.com>
2022-10-04 17:23:07 -0400

    added questions

--- js/questions.js
+++ js/questions.js
@@ -1,12 +1,34 @@
-Inside which HTML element do we put the JavaScript?
+var questions = [
+{
+title: 'Commonly useed data types DO NOT include:'
+choices: [
+    'strings,boleans,alerts,numbers'],
+answer: 'alerts',
+}
+{
+    title: 'The condition in an if/else statemnt is enclosed within__.',
+    choices:[
+        'quotes','curly brackets', 'paranthesis', 'square brackets'],
+     answer: 'parenthesis',
+}
 
-<scripting>
-<JavaScript>
-<sript>
-<js>
+{
+
+title:'Arrays in JavaScript can be ussed to store__'
+choices [
+    'numbers and strings','other arrays','boleans','all of the above'],
+
+]
+answer:'all of the above'
+
+}
+{
+
+    title: 'String values must be neclodded within___ when being assigned to variables.'
+    choices:[
+
+    ]
+    answer:
 
 
-What is the correct JavaScript syntax to change the content of the HTML element below?
-
-<p id="demo">This is a demonstration.</p>
-
+}
