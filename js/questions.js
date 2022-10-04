f76aa579b61371aea0d11a9a4ba756b800f41ea5
LynnPellitteri <lynnpellitteri@gmail.com>
2022-10-04 16:52:46 -0400

    added templates

---
+++ js/questions.js
@@ -0,0 +1,12 @@
+Inside which HTML element do we put the JavaScript?
+
+<scripting>
+<JavaScript>
+<sript>
+<js>
+
+
+What is the correct JavaScript syntax to change the content of the HTML element below?
+
+<p id="demo">This is a demonstration.</p>
+
